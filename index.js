ca86a81f2d8591833d8f2494baaa40ac9a1ab0e1
Andrei B <ruby.project15@gmail.com>
2021-02-23 13:32:03 +0200

    vidley v1.0

---
+++ index.js
@@ -0,0 +1,96 @@
+let express = require('express');
+const Joi = require('joi');
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+
+const genres = [
+    { id: 1, name: 'Action' },  
+    { id: 2, name: 'Horror' },  
+    { id: 3, name: 'Romance' },  
+  ];
+
+
+
+// @desc    Get all genres
+// @route   GET /api/genres/
+// @access  Public
+app.get('/api/genres', (req, res) =>{
+    //return all genres
+    res.send(genres);
+    
+})
+
+
+// @desc    Get a genre by id
+// @route   GET /api/genres/
+// @access  Public
+app.get('/api/genres/:id', (req, res) =>{
+    //find genre
+    const result = genres.find( gen => gen.id === parseInt(req.params.id));
+    if(!result) res.status(404).send(`Genre with ID: ${req.params.id} was not found`)
+    res.send(result)
+})
+
+
+// @desc    Post a new genre
+// @route   POST /api/genres
+// @access  Public
+app.post('/api/genres', (req, res) => {
+    //validate genre
+    const { error } = validateGenre(req.body)
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //ompose the genre to be pussed to the array
+    genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+
+    //add genre to array and return response
+    genres.push(genre);
+    res.send(genre);
+})
+
+
+// @desc    Update a genre by id
+// @route   PUT /api/genres/id
+// @access  Public
+app.put('/api/genres/:id', (req, res) =>{
+    //find genre
+    const result = genres.find( gen => gen.id === parseInt(req.params.id));
+    if(!result) return res.status(404).send(`Genre with ID: ${req.params.id} was not found`);
+
+    //validate new data
+    const { error } = validateGenre(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //update
+    result.name = req.body.name;
+    res.send(result);
+})
+
+
+// @desc    Delete a genre by id
+// @route   DELETE /api/genres/id
+// @access  Public
+app.delete('/api/genres/:id', (req, res) =>{
+    //find genre
+    const result = genres.find( gen => gen.id === parseInt(req.params.id))
+    if(!result) return res.status(400).send(`Genre with ID: ${req.params.id} was not found`);
+
+    //delete it
+    const index = genres.indexOf(result);
+    genres.splice(index, 1);
+    res.send(`deleted genre with ID: ${result.id} and name: ${result.name}`);
+})
+
+
+function validateGenre(genre){
+    const schema = Joi.object({name: Joi.string().min(3).required()});
+    return schema.validate(genre);
+}
